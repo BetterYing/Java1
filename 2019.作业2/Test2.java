731a89788eb5307c1827b2891539b61cbcb55943
BetterYing <876264306@qq.com>
2019-09-17 21:14:44 +0800

    homework

---
+++ 2019.作业2/Test2.java
@@ -0,0 +1,15 @@
+// 判定一个数字是否是素数
+//打印1-100之间的素数
+
+public class Test2{
+    public static void main(String[] args){
+        int i = 0;
+        
+        for(i = 1;i <= 100;i++){
+            
+
+       
+
+        }
+    }
+}
