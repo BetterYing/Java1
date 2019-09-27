568c582ae7f764df0f9d71aba209c6880f1118d9
BetterYing <876264306@qq.com>
2019-09-27 23:53:14 +0800

    jilu

---
+++ 20190927/src/Calculator.java
@@ -0,0 +1,18 @@
+//实现加减乘除四种运算
+
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args){
+        int num1;
+        int num2;
+        Scanner sc = new Scanner(System.in);
+
+    }
+    public static int add(int a, int b){
+        return  a + b;
+    }
+    public static int
+
+
+}
