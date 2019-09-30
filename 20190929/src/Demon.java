82892ef0a358c4273bc9266d3de3baa87a391e48
BetterYing <876264306@qq.com>
2019-09-30 12:44:25 +0800

    linkedlist

---
+++ 20190929/src/Demon.java
@@ -0,0 +1,54 @@
+public class Demon {
+    public ListNode  reverseList(ListNode head){
+        if (head == null){
+            return null;
+        }
+        if (head.next == null){
+            return head;
+        }
+        ListNode newHead = null;
+        ListNode cur = head;
+        ListNode prev = null;
+        while (cur != null){
+            ListNode next = cur.next;
+            if (next == null){
+                newHead = cur;
+            }
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return newHead;
+    }
+
+    public ListNode middleNode(ListNode head){
+        int steps = size(head) / 2;
+        ListNode cur = head;
+        for (int i = 0; i < steps; i++) {
+            cur = cur.next;
+        }
+        return cur;
+    }
+    public ListNode FindKthToTail(ListNode head,int k){
+        int len = size(head);
+        if (head == null || k <= 0 || k > len){
+            return null;
+        }
+        int offset = len - k;
+        ListNode cur = head;
+        for (int i = 0; i < offset; i++) {
+            cur = cur.next;
+        }
+        return cur;
+    }
+
+
+    public int size(ListNode head){
+        int size = 0;
+        for (ListNode cur = head;cur != null;cur = cur.next){
+            size++;
+        }
+        return size;
+    }
+
+}
