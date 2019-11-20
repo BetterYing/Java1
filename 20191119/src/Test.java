4290f5ddd2981bf876d2cc19e6ea046e71be3861
BetterYing <876264306@qq.com>
2019-11-20 15:08:46 +0800

    jiLu

--- 20191119/src/Test.java
+++ 20191119/src/Test.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
-
 public class Test {
     public static void main (String[] args) {
         Scanner sc = new Scanner(System.in);
         while(sc.hasNext()){
             String str1=sc.next();
             String str2=sc.next();
-            int ret = func(str1,str2);
+            int ret = func(str1,str2);//测试是否包含
             System.out.println(ret);
         }
     }
+
+
     public static int func(String str1,String str2) {
         if(str1.contains(str2) || str2.contains(str1)) {
             return 1;
