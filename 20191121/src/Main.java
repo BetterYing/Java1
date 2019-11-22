102f3e8cb6ad5f2805d9e885b46380b4692f147f
BetterYing <876264306@qq.com>
2019-11-22 13:12:50 +0800

    jiLu

---
+++ 20191121/src/Main.java
@@ -0,0 +1,23 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int val) {
+        this.val = val;
+    }
+}
+
+public class Main {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int ret = maxDepth(root.left);
+        int ret1 = maxDepth(root.right);
+        return ret > ret1 ? ret + 1 : ret1 + 1;
+    }
+    
+
+
+
+}
