f13017ef64c2555c2289c14997ba574e578f16d3
BetterYing <876264306@qq.com>
2019-12-11 15:25:50 +0800

    try Botton

---
+++ 20191210/src/sample/Controller.java
@@ -0,0 +1,11 @@
+package sample;
+
+import javafx.fxml.FXML;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    @FXML public void onButton(ActionEvent actionEvent) {
+        System.out.println("hello world");
+    }
+}
