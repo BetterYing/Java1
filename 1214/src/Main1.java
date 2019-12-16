8f258c77b8c9e20b14d4cf6fc6a6e34ba80b58ea
BetterYing <876264306@qq.com>
2019-12-16 16:31:26 +0800

    question

--- 1214/src/Main1.java
+++ 1214/src/Main1.java
@@ -1,8 +1,21 @@
+import java.util.Stack;
+
 public class Main1 {
+    Stack<Integer> stack1 = new Stack<>();
+    Stack<Integer> stack2 = new Stack<>();
 
+    public void push(int node) {
+        stack1.add(node);
 
-
-    public static void main(String[] args){
-
+    }
+    public int pop() {
+        while (!stack1.empty()) {
+            stack2.add(stack1.pop());
+        }
+        int ret = stack2.pop();
+        while (!stack2.empty()) {
+            stack1.add(stack2.pop());
+        }
+        return ret;
     }
 }
