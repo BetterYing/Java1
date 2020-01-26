4762658ebca8695f58686921d35fb64aaede5368
BetterYing <876264306@qq.com>
2020-01-26 19:36:00 +0800

    linked renew

---
+++ 20200126/src/Main.java
@@ -0,0 +1,35 @@
+class Node {
+    public int data;
+    public Node next;
+    public Node(int data) {
+        this.data = data;
+    }
+}
+
+public class Main {
+    public Node head;
+    public Node merge (Node head1,Node head2) {
+        Node newHead = new Node(0);
+        Node tail = newHead;
+
+        while (head1 != null && head2 != null) {
+            if (head1.data < head2.data) {
+                tail.next = head1;
+                tail = tail.next;
+                head1 = head1.next;
+            } else {
+                tail.next = head2;
+                tail = tail.next;
+                head2 = head2.next;
+            }
+        }
+        if (head1 == null) {
+            tail.next = head2;
+        }
+        if (head2 == null) {
+            tail.next = head1;
+        }
+        return newHead.next;
+    }
+
+}
