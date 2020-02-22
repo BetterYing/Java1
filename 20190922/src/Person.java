d8d7ab52b503368a2f4f244c2d42d7ebe3aebb1a
BetterYing <876264306@qq.com>
2020-02-22 23:28:19 +0800

    renew

--- 20190922/src/Person.java
+++ 20190922/src/Person.java
@@ -6,12 +6,10 @@ public class Person {
     public void show(){
         System.out.println("我叫" + name + ", " + sex + ",今年" + age + "岁，最喜欢的人是薛花");
     }
-
     public void eat(){
         System.out.println("吃饭！");
     }
     public void sleep(){
         System.out.println("睡觉！");
     }
-
 }
