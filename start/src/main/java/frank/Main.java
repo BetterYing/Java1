c475237f55cb23441ad2a234f896ae68c1639914
BetterYing <876264306@qq.com>
2020-03-03 23:10:13 +0800

    renew

--- start/src/main/java/frank/Main.java
+++ start/src/main/java/frank/Main.java
@@ -7,7 +7,6 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new
                 ClassPathXmlApplicationContext("applications.xml");
-// 新增这里
         Object o = context.getBean("myName");
         System.out.println(o.getClass());
         System.out.println(o);
